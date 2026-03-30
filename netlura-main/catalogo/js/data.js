27918f5f50a564d759a076876d06eb56393cb5f8
Arthur Santos <santosteixeira2807@gmail.com>
2026-03-29 22:17:46 -0300

    feat: adiciona arquivos do projeto

---
+++ netlura-main/catalogo/js/data.js
@@ -0,0 +1,90 @@
+export const categories = [
+    //PRIMEIRA FILEIRA DO ARTHUR
+    {
+        title: "Continuar assistindo como Arthur",
+        owner: "Arthur",
+        items: [
+            { img: "https://novaeranews.com.br/wp-content/uploads/2025/04/O-Eternauta.jpg", youtube: "https://www.youtube.com/watch?v=...", progress: 50 },
+            { img: "https://i.ytimg.com/vi/UYnmdegYBTM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBlnKmC10z3b48_jg_Z1QQXaZYTQA", youtube: "https://www.youtube.com/watch?v=...", progress: 20 },
+            { img: "https://is1-ssl.mzstatic.com/image/thumb/Video114/v4/39/de/0a/39de0a94-7f3e-d263-6cf3-d7879ba2b0f1/pr_source.png/1200x675.jpg", youtube: "https://www.youtube.com/watch?v=...", progress: 59 },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFHk1ZC_yFeyl9Ij3ny1COqTGix7byBf7KJA&s", youtube: "https://www.youtube.com/watch?v=...", progress: 10 },
+        ]
+    }, 
+    //SEGUNDA FILEIRA
+    {
+        title: "Filmes do Arthur",
+        owner: "Arthur",
+        items: [
+            { img: "https://img.englishcinemabarcelona.com/nKizXKvqQfZzAMBylGXBi7TuU37mFVp7Mb9phhtftSw/resize:fill:800:450:1:0/gravity:sm/aHR0cHM6Ly9leHBhdGNpbmVtYXByb2QuYmxvYi5jb3JlLndpbmRvd3MubmV0L2ltYWdlcy82MGMzNzFhMy0yNzQyLTQwZWYtYTQwOS1kMzE0NmI0YTNlNDQuanBn.jpg", youtube: "https://www.youtube.com/watch?v=i6avfCqKcQo" },
+            { img: "https://i.ytimg.com/vi/rohMd1FtGaY/maxresdefault.jpg", youtube: "https://www.youtube.com/watch?v=lw1iBuaULq0" },
+            { img: "https://edu.ieee.org/br-ufcgras/wp-content/uploads/sites/243/Maze-Runner-scaled.jpg", youtube: "https://www.youtube.com/watch?v=ql4epTOWdNg" },
+            { img: "https://resenhandosonhos.com/wp-content/uploads/2017/10/blade-runner-01.jpg", youtube: "https://www.youtube.com/watch?v=xGwe7D0RKWc" },
+            { img: "https://ovicio.com.br/wp-content/uploads/thanos-3.png", youtube: "https://www.youtube.com/watch?v=t_ULBP6V9bg" },
+        ]
+    },
+    //TERCEIRA FILEIRA DO ARTHUR
+    {
+        title: "Series do Arthur",
+        owner: "Arthur",
+        items: [
+            { img: "https://episodemag.com/wp-content/uploads/2025/08/Pluribus_Poster_0101-1.jpg", youtube: "https://www.youtube.com/watch?v=tLi2gk4zV8c" },
+            { img: "https://www.seudinheiro.com/uploads/2025/03/ruptura-serie-apple-tv-streaming.jpg", youtube: "https://www.youtube.com/watch?v=EFjc_qHrnsQ" },
+            { img: "https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/85304759-f692-11ee-aa78-d602bea5d5c0/fallout+poster.png", youtube: "https://www.youtube.com/watch?v=y6wNAfGz-us" },
+            { img: "https://occ-0-8407-2218.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABWU0MH8WKhCO_lBUBTeJ2F9sXR1U7FYJUFW8FEd5nNXnJN86Zwr4LVwqsonLzNkiK40qOIa-xGU9KcN28iq1zYsBbMcX-LU1bH70.jpg?r=c0d", youtube: "https://www.youtube.com/watch?v=s9ZZLDCwptw" },
+            { img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgpqcbgKvcscGVeir0hI_1PM8W1MWB1NVWjRn8K3P4o8-ycQWlrJ0l4goaDG_UOdH0w21fzMgjo2rZ49Nj6uH1vmgbPLM2IR-KEzxE81NCb3bodROHCBXeCjCfuP76k78-zXnzgwEulRpY/s1600/Imagem-da-s%25C3%25A9rie-Dark.jpg", youtube: "https://www.youtube.com/watch?v=ESEUoa-mz2c" },
+        ]
+    },
+    //QUARTA FILEIRA
+    {
+        title: "Animes do Arthur",
+        owner: "Arthur",
+        items: [
+            { img: "https://static.wikia.nocookie.net/onepiece/images/8/87/One_Piece_Anime_Logo.png/revision/latest/scale-to-width-down/1200?cb=20250813211750&path-prefix=pt", youtube: "https://www.youtube.com/watch?v=YnczpEoeaDM" },
+            { img: "https://www.multiversos.com.br/wp-content/uploads/2020/08/vinland-saga-capa-scaled.jpg", youtube: "https://www.youtube.com/watch?v=f8JrZ7Q_p-8" },
+            { img: "https://wallpapers.com/images/hd/cowboy-bebop-characters-l75xc78ajgsjifmv.jpg", youtube: "https://www.youtube.com/watch?v=EL-D9LrFJd4&list=RDEL-D9LrFJd4&start_radio=1" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFwMQJFrqy0t4J3wnrVve--J4_oa5u7lzxNQ&s", youtube: "https://www.youtube.com/watch?v=faqmNf_fZlE&list=RDfaqmNf_fZlE&start_radio=1" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/5f4a8e3153e279f5cf0ce69b98a8deb97944925d04704a66afd46b69cd71c7f1.jpg", youtube: "https://www.youtube.com/watch?v=CFjI21M9wZs&list=RDCFjI21M9wZs&start_radio=1" },
+        ]
+    },
+    // --- FILEIRA DO JOÃO ---
+    {
+        title: "Continue Assistindo como João Vitor",
+        owner: "João Vitor",
+        items: [
+            { img: "https://link-da-imagem-acao.jpg", youtube: "https://www.youtube.com/watch?v=..." },
+            { img: "https://link-da-imagem-acao2.jpg", youtube: "https://www.youtube.com/watch?v=..." },
+            { img: "https://link-da-imagem-acao3.jpg", youtube: "https://www.youtube.com/watch?v=..." },
+            { img: "https://link-da-imagem-acao4.jpg", youtube: "https://www.youtube.com/watch?v=..." },
+
+            //Continuar adicionando mais itens conforme necessário
+
+        ]
+    },
+    // --- FILEIRA DO MATEUS ---
+    {
+        title: "Continue Assistindo como Mateus",
+        owner: "Mateus",
+        items: [
+            { img: "https://link-da-imagem-terror.jpg", youtube: "https://www.youtube.com/watch?v=..." },
+            { img: "https://link-da-imagem-terror2.jpg", youtube: "https://www.youtube.com/watch?v=..." },
+            { img: "https://link-da-imagem-terror3.jpg", youtube: "https://www.youtube.com/watch?v=..." },
+            { img: "https://link-da-imagem-terror4.jpg", youtube: "https://www.youtube.com/watch?v=..." },
+
+            //Continuar adicionando mais itens conforme necessário
+        ]
+    },
+    // --- FILEIRA Do JOAO GABRIEL  ---
+    {
+        title: "Continue Assistindo como João Gabriel",
+        owner: "João Gabriel",
+        items: [
+            { img: "https://link-da-imagem-documentario.jpg", youtube: "https://www.youtube.com/watch?v=..." },
+            { img: "https://link-da-imagem-documentario2.jpg", youtube: "https://www.youtube.com/watch?v=..." },
+            { img: "https://link-da-imagem-documentario3.jpg", youtube: "https://www.youtube.com/watch?v=..." },
+            { img: "https://link-da-imagem-documentario4.jpg", youtube: "https://www.youtube.com/watch?v=..." },
+
+            //Continuar adicionando mais itens conforme necessário
+        ]
+    },
+
+];
---
+++ netlura-main/catalogo/js/main.js
@@ -0,0 +1,29 @@
+import { categories } from './data.js';
+import { createCarousel } from './components/Carousel.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const nomePerfil = localStorage.getItem('perfilAtivoNome');
+    const imagemPerfil = localStorage.getItem('perfilAtivoImagem');
+
+    if (nomePerfil && imagemPerfil) {
+        const kidsLink = document.querySelector('.kids-link');
+        const profileIcon = document.querySelector('.profile-icon');
+        
+        if (kidsLink) kidsLink.textContent = nomePerfil;
+        if (profileIcon) profileIcon.src = imagemPerfil;
+    }
+
+    const container = document.getElementById('main-content');
+    
+    if (container) {
+        // --- AQUI ESTÁ A MUDANÇA ---
+        // Filtramos as categorias: só aparecem as que têm o nome do dono IGUAL ao perfil logado
+        const categoriasDoUsuario = categories.filter(cat => cat.owner === nomePerfil);
+
+        categoriasDoUsuario.forEach(category => {
+            const carousel = createCarousel(category);
+            container.appendChild(carousel);
+        });
+        // ---------------------------
+    }
+});
